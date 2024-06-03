1e9109ab29db750d4496836e6ac65e6b5f176188
James Blackburn <blackburnfjames@gmail.com>
2024-06-03 16:34:49 +0100

    refactor: Move workbook creation to parent

--- renderer/components/pipeline.tsx
+++ renderer/components/pipeline.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import FileSelectionStage from "./file_selection_stage";
 import WorksheetSelectionStage from "./worksheet_selection_stage";
 import CalculateStage from "./calculate_stage";
+import {WorkBook} from "xlsx";
 
 export default function Pipeline() {
     const [selectedFile, setSelectedFile] = React.useState<File>();
-    const [selectedWorksheet, setSelectedWorksheet] = React.useState<boolean>();
+    const [workbook, setWorkbook] = React.useState<WorkBook>();
+
+    useEffect(() => {
+        if (selectedFile) {
+            window.electron.readFile(selectedFile.path).then((workbook: WorkBook) => {
+                setWorkbook(workbook);
+            });
+        } else {
+            setWorkbook(null);
+        }
+    }, [selectedFile]);
 
     return (
         <React.Fragment>
             <FileSelectionStage setSelectedFile={setSelectedFile}></FileSelectionStage>
-            <WorksheetSelectionStage selectedFile={selectedFile}></WorksheetSelectionStage>
-            <CalculateStage isEnabled={selectedWorksheet}></CalculateStage>
+            <WorksheetSelectionStage workbook={workbook}></WorksheetSelectionStage>
+            <CalculateStage isEnabled={false}></CalculateStage>
         </React.Fragment>
     )
 }
--- renderer/components/worksheet_selection_stage.tsx
+++ renderer/components/worksheet_selection_stage.tsx
@@ -1,30 +1,16 @@
 import React, {useEffect} from "react";
 import {WorkBook} from "xlsx";
 
-export default function WorksheetSelectionStage(props: { selectedFile: File }) {
-    const [workbook, setWorkbook] = React.useState<WorkBook>();
-
-    useEffect(() => {
-        if (props.selectedFile) {
-            window.electron.readFile(props.selectedFile.path).then((workbook: WorkBook) => {
-                setWorkbook(workbook);
-            })
-        } else {
-            setWorkbook(null);
-        }
-    }, [props.selectedFile]);
-
-
+export default function WorksheetSelectionStage(props: { workbook: WorkBook }) {
     return (
         <React.Fragment>
             <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select
                 the worksheet</label>
-            <select defaultValue={0} id="countries" disabled={workbook === null}
+            <select defaultValue={0} id="countries" disabled={props.workbook === null}
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 <option>Choose a worksheet</option>
-                {workbook && workbook.SheetNames.map(sheetName => {
-                    return <option value={sheetName} key={sheetName}>{sheetName}</option>;
-                })}
+                {props.workbook && props.workbook.SheetNames.map(sheetName => <option value={sheetName}
+                                                                                      key={sheetName}>{sheetName}</option>)}
             </select>
         </React.Fragment>
     );
