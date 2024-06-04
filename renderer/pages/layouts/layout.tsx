5953908dd8ee24c9057a053c52a3c7784df0c58b
James Blackburn <blackburnfjames@gmail.com>
2024-06-04 12:56:51 +0100

    feat: Add bottom rounded edge

--- renderer/pages/layouts/layout.tsx
+++ renderer/pages/layouts/layout.tsx
@@ -5,7 +5,7 @@ import logo from "../../../resources/icon.svg";
 export default function Layout(props: PropsWithChildren) {
   const { children } = props;
   return (
-    <div className="flex flex-col h-full">
+    <div className="flex flex-col h-full rounded-lg">
       <header className="shadow-md">
         <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
@@ -18,6 +18,27 @@ export default function Layout(props: PropsWithChildren) {
               <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
                 SenPAQ Tally
               </span>
+            </span>
+            <span className="flex items-center">
+              <button className="w-6 h-6 text-gray-800 dark:text-white">
+                <svg
+                  className="w-6 h-6 text-gray-800 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18 17.94 6M18 18 6.06 6"
+                  />
+                </svg>
+              </button>
             </span>
           </div>
         </nav>
